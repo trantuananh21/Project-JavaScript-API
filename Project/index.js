97d0575a0ed56d7214741a63e4b94fa759f2015b
trantuananh21 <109894389+trantuananh21@users.noreply.github.com>
2022-10-22 14:28:42 +0200

    fix lỗi async, await

--- Project/index.js
+++ Project/index.js
@@ -1,12 +1,18 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'b6e5c43dcamsh0acfaea2676ac9fp11af8djsn7de1bffb9d03',
-		'X-RapidAPI-Host': 'unogs-unogs-v1.p.rapidapi.com'
-	}
-};
+const netflix = async() => {
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'b6e5c43dcamsh0acfaea2676ac9fp11af8djsn7de1bffb9d03',
+			'X-RapidAPI-Host': 'unogs-unogs-v1.p.rapidapi.com'
+		}
+	};
+	
+	var response = ''
 
-fetch('https://unogs-unogs-v1.p.rapidapi.com/search/titles?order_by=date&type=movie', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	response = await fetch('https://unogs-unogs-v1.p.rapidapi.com/search/titles?order_by=date&type=movie', options)
+
+	data = await response.json()
+
+	console.log(data);
+}
+netflix();

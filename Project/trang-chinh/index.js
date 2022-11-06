9c3a64a9ab49fc8bd5e0a4320940480fa6e344df
trantuananh21 <109894389+trantuananh21@users.noreply.github.com>
2022-11-06 15:21:02 +0100

    upload project (6/11)

--- Project/trang-chinh/index.js
+++ Project/trang-chinh/index.js
@@ -21,22 +21,43 @@ const netflix = async() => {
 	film.forEach(element => {
 		console.log(element);
 		var imgLink = element.img
+		var name = element.title
+		var day = element.title_date
+		var description = element.synopsis
 		count++
 
 		img = document.createElement('img')
+		title = document.createElement('h3')
+		date = document.createElement('p')
+		content = document.createElement('p')
 
 		img.src = imgLink
+		title.innerHTML = name
+		date.innerHTML = day
+		content.innerHTML = description
 
 		if (count >= 1 & count <= 40){	
-			document.getElementById('row-poster1').appendChild(img)
+			document.getElementById('images1').appendChild(img)
+			document.getElementById('title1').appendChild(title)
+			document.getElementById('date1').appendChild(date)
+			document.getElementById('description1').appendChild(content)
+
 		}
 		if (count >= 41 & count <= 80){
-				document.getElementById('row-poster2').appendChild(img)
+			document.getElementById('images2').appendChild(img)
+			document.getElementById('title2').appendChild(title)
+			document.getElementById('date2').appendChild(date)
+			document.getElementById('description2').appendChild(content)
 		}
 
 		if (count >= 81 & count <= 100){
-			document.getElementById('row-poster3').appendChild(img)
+			document.getElementById('images3').appendChild(img)
+			document.getElementById('title3').appendChild(title)
+			document.getElementById('date3').appendChild(date)
+			document.getElementById('description3').appendChild(content)
 		}
+
+
 	});
 }
 netflix();
